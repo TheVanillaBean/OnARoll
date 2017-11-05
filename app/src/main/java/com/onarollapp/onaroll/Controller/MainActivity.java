07bf99c7ee6d37aeab8a71c5820efd58485ebd1f
Alex Alimov <teambluepin@gmail.com>
2017-11-05 02:15:37 -0700

    Add dependencies and create folder structure

---
+++ app/src/main/java/com/onarollapp/onaroll/Controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.onarollapp.onaroll.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onarollapp.onaroll.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
