ab85a3e43909be1f8b8147c0d5f868c84c98c573
Curtis Sapp <curtmsapp@gmail.com>
2017-11-12 10:28:58 -0700

    Refamilizarizing myself with Android
    Its not much but maybe it will motivate other people to start working on the project.

--- app/src/main/java/com/onarollapp/onaroll/Controller/MainActivity.java
+++ app/src/main/java/com/onarollapp/onaroll/Controller/MainActivity.java
@@ -1,7 +1,9 @@
 package com.onarollapp.onaroll.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.onarollapp.onaroll.R;
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToShop(View v) {
+        Intent intent = new Intent(this, ShopActivity.class);
+
+        startActivity(intent);
     }
 }
