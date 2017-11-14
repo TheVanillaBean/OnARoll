d2c5413cd14b7cba4ae2db7120cf14725539b348
Curtis Sapp <curtmsapp@gmail.com>
2017-11-14 13:17:05 -0700

    Added Player Model

---
+++ app/src/main/java/com/onarollapp/onaroll/Users/Player.java
@@ -0,0 +1,17 @@
+package com.onarollapp.onaroll.Users;
+
+import com.onarollapp.onaroll.Perks.Perk;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by Curt on 11/14/2017.
+ */
+
+public class Player {
+    public String id;
+    public int credits;
+
+    public ArrayList<Perk> perkList;
+}
