034f1269afc288ed0ad42bc50629d43b22a5fdc4
Curtis Sapp <curtmsapp@gmail.com>
2017-11-14 12:51:32 -0700

    Added fields to GameState Object

--- app/src/main/java/com/onarollapp/onaroll/Logic/GameState.java
+++ app/src/main/java/com/onarollapp/onaroll/Logic/GameState.java
@@ -1,12 +1,36 @@
 package com.onarollapp.onaroll.Logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Curt on 11/13/2017.
  */
 
 public class GameState {
-    RollResult lastRoll;
+    ArrayList<RollResult> rollHistory;
+    String playerOneId;
+    String playerTwoId;
+    boolean isPlayerOneTurn;
+
+    int playerOneTotalScore;
+    int playerTwoTotalScore;
+    int playerOneLastRunningScore;
+    int playerTwoLastRunningScore;
+
+    int currentPlayerRunningScore;
 
 
+    public RollResult getMostRecentRoll() {
+        return rollHistory.get(rollHistory.size() - 1);
+    }
+
+    public void updateMostRecentRoll(RollResult newResult) {
+        rollHistory.set(rollHistory.size() - 1, newResult);
+    }
+
+    public void addNewRoll(RollResult newRoll) {
+        rollHistory.add(newRoll);
+    }
 
 }
