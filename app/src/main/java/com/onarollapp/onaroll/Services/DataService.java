6f16f3efc824ead790844eea1410f1c24f30d786
Curtis Sapp <curtmsapp@gmail.com>
2017-11-14 13:16:15 -0700

    Added DataService, contains method stubs for Alex to fill out

---
+++ app/src/main/java/com/onarollapp/onaroll/Services/DataService.java
@@ -0,0 +1,33 @@
+package com.onarollapp.onaroll.Services;
+
+import com.onarollapp.onaroll.Perks.Perk;
+import com.onarollapp.onaroll.ShopModels.PerkShopItem;
+import com.onarollapp.onaroll.ShopModels.ShopItem;
+import com.onarollapp.onaroll.Users.Player;
+
+/**
+ * Created by Curt on 11/14/2017.
+ */
+
+public class DataService {
+
+    // Get Player object from DB by ID
+    public static Player getPlayer(String playerId) {
+        return null;
+    }
+
+    // Update Player object in DB
+    public static void updatePlayer(Player player) {
+
+    }
+
+    // Get ShopItem object from DB by ID
+    public static ShopItem getShopItem(String shopId) {
+        return null;
+    }
+
+    // Get Perk object from DB by ID
+    public static Perk getPerk(String perkId) {
+        return null;
+    }
+}
