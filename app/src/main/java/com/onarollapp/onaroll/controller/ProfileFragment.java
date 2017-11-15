58b28392aa2d9638edfba6a075be5ea9fb3de27f
Alex Alimov <teambluepin@gmail.com>
2017-11-15 02:48:11 -0700

    Add Profile Fragment Logic/Design

--- app/src/main/java/com/onarollapp/onaroll/controller/ProfileFragment.java
+++ app/src/main/java/com/onarollapp/onaroll/controller/ProfileFragment.java
@@ -1,18 +1,44 @@
 package com.onarollapp.onaroll.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.onarollapp.onaroll.DataService.AuthService;
+import com.onarollapp.onaroll.POJO.UserCastEvent;
 import com.onarollapp.onaroll.R;
+import com.onarollapp.onaroll.model.User;
+import com.onarollapp.onaroll.util.Dialog;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class ProfileFragment extends Fragment {
+
+    @BindView(R.id.user_image) ImageView mProfileImage;
+    @BindView(R.id.name_label) TextView mNameLabel;
+    @BindView(R.id.email_label) TextView mEmailLabel;
+
+    private User mCurrentUser;
+    private TextDrawable mTextDrawable;
+    private ColorGenerator generator;
+    private int randomColor;
+
     public ProfileFragment() {
     }
 
@@ -37,6 +63,49 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        if(AuthService.getInstance().getCurrentUser() != null){
+            generator = ColorGenerator.MATERIAL;
+            User.castUser(AuthService.getInstance().getCurrentUser().getUid());
+        }else{
+            Dialog.showDialog(getActivity(), "Authentication Error", "Could not find user...", "Okay");
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onUserCastCallBack(UserCastEvent event) {
+        if (event.getError() == null){
+            mCurrentUser = event.getUser();
+            mNameLabel.setText(mCurrentUser.getFullName());
+            mEmailLabel.setText(mCurrentUser.getEmail());
+            randomColor = generator.getRandomColor();
+            mTextDrawable = TextDrawable.builder()
+                    .buildRound(mCurrentUser.getEmail().substring(0,2), randomColor);
+            mProfileImage.setImageDrawable(mTextDrawable);
+        }else{
+            Dialog.showDialog(getActivity(), "Authentication Error", event.getError(), "Okay");
+        }
+    }
+
+    @OnClick(R.id.logout_btn)
+    public void onLogoutBtnPressed() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
     }
 
 }
