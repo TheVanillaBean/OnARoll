fbad74651a599146809ad6be661604098efd93ce
Curtis Sapp <curtmsapp@gmail.com>
2017-11-30 23:56:03 -0700

    Added shop endpoint to data service

--- app/src/main/java/com/onarollapp/onaroll/Services/DataService.java
+++ app/src/main/java/com/onarollapp/onaroll/Services/DataService.java
@@ -1,9 +1,12 @@
 package com.onarollapp.onaroll.Services;
 
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollFive;
 import com.onarollapp.onaroll.Perks.Perk;
 import com.onarollapp.onaroll.ShopModels.PerkShopItem;
 import com.onarollapp.onaroll.ShopModels.ShopItem;
 import com.onarollapp.onaroll.Users.Player;
+
+import java.util.ArrayList;
 
 /**
  * Created by Curt on 11/14/2017.
@@ -22,7 +25,10 @@ public class DataService {
 
     // Return the player object of the user currently logged in
     public Player getLoggedInPlayer() {
-        return null;
+        Player player = new Player();
+        player.credits = 200;
+        player.id = "2";
+        return player;
     }
     // Get Player object from DB by ID
     public Player getPlayer(String playerId) {
@@ -37,6 +43,31 @@ public class DataService {
     // Get ShopItem object from DB by ID
     public ShopItem getShopItem(String shopId) {
         return null;
+    }
+
+    // Get all purchaseable shop items
+    public ArrayList<ShopItem> getShopItems() {
+        ArrayList<ShopItem> shopItems = new ArrayList<ShopItem>();
+        shopItems.add(new PerkShopItem(new IncreaseChanceToRollFive(), 100, "21"));
+        return null;
+    }
+
+    // Buy ShopItem for logged in player
+    // Return success or not
+    public boolean buyPerkShopItem(String shopId) {
+        return this.buyPerkShopItem((PerkShopItem)this.getShopItem(shopId));
+    }
+
+    public boolean buyPerkShopItem(PerkShopItem shopItem) {
+        Player player = this.getLoggedInPlayer();
+        if(player.credits >= shopItem.price) {
+            player.credits = player.credits - shopItem.price;
+            player.perkList.add(shopItem.perk);
+            this.updatePlayer(player);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Get Perk object from DB by ID
