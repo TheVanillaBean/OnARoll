700f1406ec15532271e445834defe10a191b8e76
Curtis Sapp <curtmsapp@gmail.com>
2017-11-30 23:55:48 -0700

    Building shop  UI and logic

--- app/src/main/java/com/onarollapp/onaroll/controller/ShopActivity.java
+++ app/src/main/java/com/onarollapp/onaroll/controller/ShopActivity.java
@@ -1,31 +1,103 @@
 package com.onarollapp.onaroll.controller;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.onarollapp.onaroll.Perks.EmptyPerk;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollFive;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollFour;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollOne;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollSix;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollThree;
+import com.onarollapp.onaroll.Perks.IncreaseChanceToRollTwo;
+import com.onarollapp.onaroll.Perks.Perk;
+import com.onarollapp.onaroll.Perks.PerkListItem;
+import com.onarollapp.onaroll.Perks.RollOneExtraDie;
 import com.onarollapp.onaroll.R;
+import com.onarollapp.onaroll.Services.DataService;
+import com.onarollapp.onaroll.ShopModels.ShopItem;
+import com.onarollapp.onaroll.Users.Player;
+
+import java.util.ArrayList;
 
 public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_perk_customization);
 
-        Button fab = (Button) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        DataService ds = DataService.getInstance();
+        ArrayList<ShopItem> shopItems = ds.getShopItems();
+        Player player = ds.getLoggedInPlayer();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Shop - $" + player.credits);
+
+        ListView yourPerkList = (ListView) findViewById(R.id.shop_list);
+
+        ShopListAdapter adapter = new ShopListAdapter(this, shopItems);
+        yourPerkList.setAdapter(adapter);
+
+
+        yourPerkList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                DataService ds = DataService.getInstance();
+                Player player = ds.getLoggedInPlayer();
+
+
             }
         });
+
+
     }
 
+
+
+
+
+    private class ShopListAdapter extends ArrayAdapter<ShopItem> {
+        private final Context context;
+        private final ArrayList<ShopItem> shopItems;
+
+        public ShopListAdapter(Context context, ArrayList<ShopItem> shopItems) {
+            super(context, -1, shopItems);
+            this.context = context;
+            this.shopItems = shopItems;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.perk_customization_list_row, parent, false);
+
+            TextView title = (TextView) rowView.findViewById(R.id.shop_item_title);
+            title.setText(shopItems.get(position).title);
+
+
+            TextView description = (TextView) rowView.findViewById(R.id.shop_item_description);
+            description.setText(shopItems.get(position).description);
+
+            Button buyButton = (Button) rowView.findViewById(R.id.shop_item_buy_button);
+            buyButton.setText("Buy " + shopItems.get(position).price);
+
+            return rowView;
+        }
+    }
+
+
 }
+
+
